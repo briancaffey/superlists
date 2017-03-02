0d5189341dbe640a513944b4c1515f95ab0e5678
mrbrian88 <briancaffey2010@gmail.com>
2017-03-02 12:11:56 -0500

    working on list functionality

--- functional_tests.py
+++ functional_tests.py
@@ -36,11 +36,9 @@ class NewVisitorTest(unittest.TestCase):
 
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
-		self.assertTrue(
-			any(row.text == '1: Buy peacock feathers' for row in rows), 
-			'New to-do item did not appear in table'
+		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
 
-			)
+		self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
 
 		self.fail('Finish the test!')
 
