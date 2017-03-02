e6a878aedf1faaaf6b95696fee2b16908105441a
mrbrian88 <briancaffey2010@gmail.com>
2017-03-02 14:51:13 -0500

    make func tests an app, use LiveServerTestCase

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,9 +1,10 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 import unittest
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 	
 	def setUp(self):
 		self.browser = webdriver.Firefox()
@@ -18,7 +19,7 @@ class NewVisitorTest(unittest.TestCase):
 
 	def test_can_start_a_list_and_retrieve_it_later(self):
 
-		self.browser.get('http://localhost:8000')
+		self.browser.get(self.live_server_url)
 
 		self.assertIn('To-Do', self.browser.title)
 		header_text = self.browser.find_element_by_tag_name('h1').text
@@ -50,6 +51,3 @@ class NewVisitorTest(unittest.TestCase):
 		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
 		self.fail('Finish the test!')
-
-if __name__ == '__main__':
-	unittest.main()
